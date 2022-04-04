3bbc7b0b6e888bef39001493659335fd8c2b7c9a
Marat Fattahov <illilois@gmx.com>
2022-04-04 18:42:25 +0000

    Revert "Merge branch '5_adding_newEntity_Category' into 'dev'"
    This reverts merge request !4

--- src/main/java/app/config/DataInitializer.java
+++ src/main/java/app/config/DataInitializer.java
@@ -1,8 +1,5 @@
 package app.config;
 
-import app.entities.Category;
-import app.services.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -15,28 +12,8 @@ import javax.annotation.PostConstruct;
 @Component
 public class DataInitializer {
 
-    public final CategoryService categoryService;
-
-    @Autowired
-    public DataInitializer(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
-
     @PostConstruct
     public void init() {
         System.out.println("DataInitializer сработал!");
-    }
-
-    /**
-     *Creating categories in the database
-     *You can add new ones here
-     */
-    @PostConstruct
-    public void createCategory() {
-        Category business = new Category("BUSINESS");
-        Category economy = new Category("ECONOMY");
-
-        categoryService.saveCategory(business);
-        categoryService.saveCategory(economy);
     }
 }
