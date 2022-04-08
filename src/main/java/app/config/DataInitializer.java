559a289f5e73e9e4e68921390c638b9b4d5e9c2d
Aristov Pavel <https://github.com/aristovp/PP_1>
2022-04-08 21:23:28 +0300

    Добавил логгер log4j2 с записью файлов в корень проекта

--- src/main/java/app/config/DataInitializer.java
+++ src/main/java/app/config/DataInitializer.java
@@ -1,5 +1,6 @@
 package app.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,11 +10,13 @@ import javax.annotation.PostConstruct;
  * Эти данные будут каждый раз создаваться заново при поднятии SessionFactory и удаляться из БД при её остановке.
  * Инжектьте и используйте здесь соответствующие сервисы ваших сущностей."
  */
+@Log4j2
 @Component
 public class DataInitializer {
 
     @PostConstruct
     public void init() {
         System.out.println("DataInitializer сработал!");
+        log.info("Тест логгера");
     }
 }
--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -1,16 +1,19 @@
 package app.controllers;
 
 import io.swagger.annotations.Api;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@Log4j2
 @RestController
 @Api(tags = "MainController")
 public class MainController {
 
     @GetMapping("/")
     public ModelAndView index() {
+        log.info("run controller \"index\" ");
         return new ModelAndView("index");
     }
 }
