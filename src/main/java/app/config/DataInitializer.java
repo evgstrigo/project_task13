6be128395166dfd8471d96da9cb97e7fcc2e7db2
Aristov Pavel <https://github.com/aristovp/PP_1>
2022-04-11 22:18:31 +0300

    Добавил логгер log4j2 с записью файлов в корень проекта. Сделал rebase для разрешения конфликтов

--- src/main/java/app/config/DataInitializer.java
+++ src/main/java/app/config/DataInitializer.java
@@ -6,9 +6,7 @@ import app.services.CategoryService;
 import app.services.PassengerService;
 import app.util.PassengerAndPassportCreator;
 import org.springframework.beans.factory.annotation.Autowired;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 
 /**
